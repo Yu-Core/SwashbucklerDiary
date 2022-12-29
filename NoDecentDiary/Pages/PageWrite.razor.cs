70604dafc44f6ce442bfcaf772ff1cae3cfe4c67
Yu-Core <3105136125@qq.com>
2022-12-30 06:28:36 +0800

    【初步添加】Write页面的布局

--- NoDecentDiary/Pages/PageWrite.razor.cs
+++ NoDecentDiary/Pages/PageWrite.razor.cs
@@ -8,5 +8,20 @@ namespace NoDecentDiary.Pages
 {
     public partial class PageWrite
     {
+        private readonly List<string> _weathers = new List<string>()
+    {
+        "晴","阴","小雨","中雨","大雨","小雪","中雪","大雪","雾",
+    };
+        private string _value;
+        private List<string> _weathers2 = new List<string>();
+
+        private void Remove(string item)
+        {
+            var index = _weathers2.IndexOf(item);
+            if (index >= 0)
+            {
+                _weathers2.RemoveAt(index);
+            }
+        }
     }
 }
