ab03dd703c9e774bec050c156b3aa321023b8a9b
Yu-Core <3105136125@qq.com>
2022-12-31 04:35:09 +0800

    【更新】Write页面的布局

--- NoDecentDiary/Pages/PageWrite.razor.cs
+++ NoDecentDiary/Pages/PageWrite.razor.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NoDecentDiary.Models;
 
 namespace NoDecentDiary.Pages
 {
     public partial class PageWrite
     {
         private readonly List<string> _weathers = new List<string>()
-    {
-        "晴","阴","小雨","中雨","大雨","小雪","中雪","大雪","雾",
-    };
-        private string _value;
-        private List<string> _weathers2 = new List<string>();
-
-        private void Remove(string item)
         {
-            var index = _weathers2.IndexOf(item);
-            if (index >= 0)
+            "晴","阴","小雨","中雨","大雨","小雪","中雪","大雪","雾",
+        };
+        private string? _weather;
+        private bool showMenu;
+        private bool showTitle;
+        private List<TagModel> SelectedTags = new List<TagModel>()
+        {
+            new TagModel(){Name="标签"}
+        };
+
+        private void RemoveSelectedTag(TagModel tag)
+        {
+            int index = SelectedTags.IndexOf(tag);
+            if (index > -1)
             {
-                _weathers2.RemoveAt(index);
+                SelectedTags.RemoveAt(index);
             }
         }
     }
