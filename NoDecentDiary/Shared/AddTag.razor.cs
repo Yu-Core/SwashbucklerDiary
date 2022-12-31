4374cbb0adedd00b0aa0ea8f23412f76550c00b7
Yu-Core <3105136125@qq.com>
2023-01-01 04:56:57 +0800

    feat(AddTag) : add AddTag

---
+++ NoDecentDiary/Shared/AddTag.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDecentDiary.Shared
+{
+    public partial class AddTag
+    {
+        [Parameter]
+        public bool Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+            }
+        }
+        [Parameter]
+        public EventCallback<bool> ValueChanged { get; set; }
+        [Parameter]
+        public string? Text { get; set; }
+        [Parameter]
+        public EventCallback<string> TextChanged { get; set; }
+        [Parameter]
+        public EventCallback OnSave { get; set; } 
+
+        private bool value;
+
+        protected virtual async Task HandleOnCancel(MouseEventArgs _)
+        {
+            await InternalValueChanged(false);
+        }
+
+        private async Task InternalValueChanged(bool value)
+        {
+            Value = value;
+
+            if (ValueChanged.HasDelegate)
+            {
+                await ValueChanged.InvokeAsync(value);
+            }
+        }
+
+        private async Task HandleOnSave()
+        {
+            await OnSave.InvokeAsync();
+        }
+    }
+}
