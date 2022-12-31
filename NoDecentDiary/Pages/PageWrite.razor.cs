e463b6cdbeb29c0b87198da6fa280e6758afada0
Yu-Core <3105136125@qq.com>
2023-01-01 05:38:20 +0800

    feat(PageWrite) : change style

--- NoDecentDiary/Pages/PageWrite.razor.cs
+++ NoDecentDiary/Pages/PageWrite.razor.cs
@@ -1,10 +1,14 @@
-﻿using NoDecentDiary.Models;
+﻿using Masa.Blazor;
+using Microsoft.AspNetCore.Components;
+using NoDecentDiary.Models;
 using System.Diagnostics;
 
 namespace NoDecentDiary.Pages
 {
     public partial class PageWrite
     {
+        [Inject]
+        public MasaBlazor? MasaBlazor { get; set; }
         private readonly List<string> _weathers = new List<string>()
         {
             "晴","阴","小雨","中雨","大雨","小雪","中雪","大雪","雾",
@@ -13,10 +17,14 @@ namespace NoDecentDiary.Pages
         private bool showMenu;
         private bool showTitle;
         private bool showSelectTag;
-        private List<TagModel> SelectedTags = new List<TagModel>()
+        private bool IsDesktop => MasaBlazor!.Breakpoint.SmAndUp;
+        private List<TagModel> SelectedTags = new List<TagModel>();
+
+        protected override Task OnInitializedAsync()
         {
-            new TagModel(){Name="标签"}
-        };
+            MasaBlazor!.Breakpoint.OnUpdate += () => { return InvokeAsync(this.StateHasChanged); };
+            return base.OnInitializedAsync();
+        }
 
         private void RemoveSelectedTag(TagModel tag)
         {
