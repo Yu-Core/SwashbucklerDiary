f3bb083bd81f2e8ae42b10b422a801e82227c378
Yu-Core <3105136125@qq.com>
2023-01-05 02:35:04 +0800

    feat(PageWriter) : add save and back

--- NoDecentDiary/Pages/PageWrite.razor.cs
+++ NoDecentDiary/Pages/PageWrite.razor.cs
@@ -1,6 +1,9 @@
-﻿using Masa.Blazor;
+﻿using BlazorComponent;
+using Masa.Blazor;
 using Microsoft.AspNetCore.Components;
+using NoDecentDiary.IServices;
 using NoDecentDiary.Models;
+using NoDecentDiary.Services;
 using System.Diagnostics;
 
 namespace NoDecentDiary.Pages
@@ -9,14 +12,20 @@ namespace NoDecentDiary.Pages
     {
         [Inject]
         public MasaBlazor? MasaBlazor { get; set; }
+        [Inject]
+        public IDiaryService? DiaryService { get; set; }
+        [Inject]
+        public IPopupService? PopupService { get; set; }
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
         private readonly List<string> _weathers = new List<string>()
         {
             "晴","阴","小雨","中雨","大雨","小雪","中雪","大雪","雾",
         };
-        private string? _weather;
         private bool showMenu;
         private bool showTitle;
         private bool showSelectTag;
+        private DiaryModel _diary = new DiaryModel();
         private bool IsDesktop => MasaBlazor!.Breakpoint.SmAndUp;
         private List<TagModel> SelectedTags = new List<TagModel>();
 
@@ -38,6 +47,47 @@ namespace NoDecentDiary.Pages
         private void HandOnSaveSelectTags()
         {
             showSelectTag = false;
+        }
+
+        private async Task HandOnSave()
+        {
+            if (string.IsNullOrWhiteSpace(_diary.Content))
+            {
+                return;
+            }
+            await AddDiary();
+        }
+
+        private async Task HandOnBack()
+        {
+            if (string.IsNullOrWhiteSpace(_diary.Content))
+            {
+                NavigationManager!.NavigateTo("/");
+                return;
+            }
+            await AddDiary();
+        }
+
+        private void HandOnClear()
+        {
+            _diary.Content = string.Empty;
+            this.StateHasChanged();
+        }
+
+        private async Task AddDiary()
+        {
+            _diary.CreateTime = DateTime.Now;
+            _diary.UpdateTime = DateTime.Now;
+            bool flag = await DiaryService!.AddAsync(_diary);
+            if (flag)
+            {
+                await PopupService!.AlertAsync("添加成功", AlertTypes.Success);
+            }
+            else
+            {
+                await PopupService!.AlertAsync("添加失败", AlertTypes.Error);
+            }
+            NavigationManager!.NavigateTo("/");
         }
     }
 }
