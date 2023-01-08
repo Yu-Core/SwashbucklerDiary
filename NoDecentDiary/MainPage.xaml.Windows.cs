aabb62d2d847ad842190fb5bee0c72bde3e2c061
Yu-Core <3105136125@qq.com>
2023-01-09 06:02:38 +0800

    fix(Scroll Bar) : Scroll Bar disable,but still show

---
+++ NoDecentDiary/MainPage.xaml.Windows.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDecentDiary
+{
+    public partial class MainPage
+    {
+        private partial void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
+        {
+        }
+
+        private partial void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
+        {
+        }
+    }
+}
---
+++ NoDecentDiary/MainPage.xaml.iOS.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.WebView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDecentDiary
+{
+    public partial class MainPage
+    {
+        private partial void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
+        {
+            e.Configuration.AllowsInlineMediaPlayback = true;
+            e.Configuration.MediaTypesRequiringUserActionForPlayback = WebKit.WKAudiovisualMediaTypes.None;
+        }
+
+        private partial void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
+        {
+            e.WebView.ScrollView.ShowsVerticalScrollIndicator = false;
+        }
+    }
+}
