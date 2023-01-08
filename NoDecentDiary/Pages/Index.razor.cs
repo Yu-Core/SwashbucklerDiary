99a0bfde35ba6c4265d1108e6801ebce1cbbcc30
Yu-Core <3105136125@qq.com>
2023-01-09 04:54:58 +0800

    feat(Index) : add tag

--- NoDecentDiary/Pages/Index.razor.cs
+++ NoDecentDiary/Pages/Index.razor.cs
@@ -19,14 +19,18 @@ namespace NoDecentDiary.Pages
         public I18n? I18n { get; set; }
         [Inject]
         public IDiaryService? DiaryService { get; set; }
+        [Inject]
+        public ITagService? TagService { get; set; }
         [CascadingParameter]
         public Error? Error { get; set; }
         private StringNumber tabs = 0;
         private List<DiaryModel> Diaries { get; set; } = new List<DiaryModel>();
+        private List<TagModel> Tags { get; set; } = new List<TagModel>();
 
         protected override async Task OnInitializedAsync()
         {
             Diaries = (await DiaryService!.QueryAsync()).Take(50).OrderByDescending(it=>it.CreateTime).ToList();
+            Tags = await TagService!.QueryAsync();
         }
     }
 }
