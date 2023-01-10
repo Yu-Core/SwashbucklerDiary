9f176ba47a9c257cd674796715d4c38acf472627
Yu-Core <3105136125@qq.com>
2023-01-10 22:53:39 +0800

    feat(Swiper) : Prevent mouse sliding

--- NoDecentDiary/wwwroot/js/init-swiper.js
+++ NoDecentDiary/wwwroot/js/init-swiper.js
@@ -5,33 +5,13 @@ function swiperInit(dotNetCallbackRef, callbackMethod,id,index) {
         observer: true,
         observeParents: true,
         observeSlideChildren: true,
-        autoHeight: true,
-        initialSlide: index,
+        autoHeight: true,//自动高度
+        simulateTouch: false,//禁止鼠标模拟
+        initialSlide: index,//设定初始化时slide的索引
         on: {
             slideChangeTransitionEnd: function () {
                 dotNetCallbackRef.invokeMethodAsync(callbackMethod, this.activeIndex);
-                //alert(this.activeIndex);//切换结束时，告诉我现在是第几个slide
             },
         }
     });
-}
-
-function changeSwiperIndex(index) {
-    swiper.slideTo(index);
-}
-
-function initSwiperForecastHours() {
-    window.forecastHoursSwiper = new Swiper('.swiper-forecast-hours', {
-        slidesPerView: 'auto',
-        touchAngle: 90,
-        freeMode: {
-            enabled: true,  
-           /* momentumRatio: 2,*/
-            momentumVelocityRatio: 2,
-        },
-    });
-}
-
-function updateSwiper() {
-    window.swiper.update();
 }
