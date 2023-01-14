c2eeddb7d525f3d17a4c277c39adcf61b14a6310
Yu-Core <3105136125@qq.com>
2023-01-14 23:52:57 +0800

    fix(ScrollContainer) : Display error when window height changes

---
+++ NoDecentDiary/Shared/ScrollContainer.razor.cs
@@ -0,0 +1,37 @@
+﻿using Masa.Blazor;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDecentDiary.Shared
+{
+    public partial class ScrollContainer : IDisposable
+    {
+        [Inject]
+        public MasaBlazor? MasaBlazor { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public string? Id { get; set; }
+        private double MaxHeight => MasaBlazor!.Breakpoint.Height - MasaBlazor.Application.Top;
+
+        public void Dispose()
+        {
+            MasaBlazor!.Breakpoint.OnUpdate -= InvokeStateHasChangedAsync;
+            GC.SuppressFinalize(this);
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            MasaBlazor!.Breakpoint.OnUpdate += InvokeStateHasChangedAsync;
+            return base.OnInitializedAsync();
+        }
+        private async Task InvokeStateHasChangedAsync()
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
