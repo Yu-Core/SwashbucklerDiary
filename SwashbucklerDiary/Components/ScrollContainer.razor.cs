3fa9dbea9885b175a732e4e5a0c0e67430a27795
Yu-Core <3105136125@qq.com>
2023-03-25 04:16:51 +0800

    🐛 fix: fixed scroll error

--- SwashbucklerDiary/Components/ScrollContainer.razor.cs
+++ SwashbucklerDiary/Components/ScrollContainer.razor.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace SwashbucklerDiary.Components
 {
-    public partial class ScrollContainer : IDisposable
+    public partial class ScrollContainer
     {
         [Inject]
         public MasaBlazor MasaBlazor { get; set; } = default!;
@@ -15,41 +15,12 @@ namespace SwashbucklerDiary.Components
         [Parameter]
         public string? Class { get; set; }
         [Parameter]
+        public string? Style { get; set; }
+        [Parameter]
         public string? ContentClass { get; set; }
         [Parameter]
-        public bool Bottom { get; set; }
-        [Parameter]
-        public double OccupyHeight { get; set; }
+        public string? ContentStyle { get; set; }
         [Parameter]
         public EventCallback OnContextmenu { get; set; }
-
-        public void Dispose()
-        {
-            MasaBlazor.Breakpoint.OnUpdate -= InvokeStateHasChangedAsync;
-            GC.SuppressFinalize(this);
-        }
-
-        protected override Task OnInitializedAsync()
-        {
-            MasaBlazor.Breakpoint.OnUpdate += InvokeStateHasChangedAsync;
-            return base.OnInitializedAsync();
-        }
-
-        private double Height
-        {
-            get
-            {
-                var height = MasaBlazor.Breakpoint.Height;
-                var top = MasaBlazor.Application.Top;
-                var bottom = Bottom ? MasaBlazor.Application.Bottom : 0;
-                return height - top - bottom - OccupyHeight;
-            }
-        }
-            
-
-        private async Task InvokeStateHasChangedAsync()
-        {
-            await InvokeAsync(StateHasChanged);
-        }
     }
 }
---
+++ SwashbucklerDiary/Components/Swiper/SwiperTabItem.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SwashbucklerDiary.Components
+{
+    public partial class SwiperTabItem
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        [Parameter]
+        public RenderFragment? FixContent { get; set; }
+    }
+}
