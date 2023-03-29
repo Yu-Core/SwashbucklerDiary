00c4decc988ad9606660891b5fb160fcfa5079af
Yu-Core <3105136125@qq.com>
2023-03-30 01:51:23 +0800

    🐛 fix: WritePage tags cannot srcoll on mobile

--- SwashbucklerDiary/wwwroot/js/vditor-helper.js
+++ SwashbucklerDiary/wwwroot/js/vditor-helper.js
@@ -1,3 +1,7 @@
+/**
+ * 点击Vditor工具栏，输入框不失去焦点
+ * @returns
+ */
 export function PreventInputLoseFocus() {
     var toolbar = document.getElementsByClassName("vditor-toolbar")[0];
     if (toolbar == null || toolbar == undefined || toolbar == "") {
