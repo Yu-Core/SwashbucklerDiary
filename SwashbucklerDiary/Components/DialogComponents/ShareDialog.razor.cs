119170a606fe1e672581fbb513b256ebdd219770
Yu-Core <3105136125@qq.com>
2023-04-06 01:36:46 +0800

    ♻️ refactor: Separate ShareDialog from ReadPage

---
+++ SwashbucklerDiary/Components/DialogComponents/ShareDialog.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using SwashbucklerDiary.Models;
+
+namespace SwashbucklerDiary.Components
+{
+    public partial class ShareDialog : DialogComponentBase
+    {
+        [Parameter]
+        public List<ViewListItem> Items { get; set; } = new();
+    }
+}
