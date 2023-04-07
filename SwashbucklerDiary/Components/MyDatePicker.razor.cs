dcceac8334dfc6e649087e19200306cd46838f9b
Yu-Core <3105136125@qq.com>
2023-04-08 03:12:02 +0800

    🆕 feat: add MyDatePicker

---
+++ SwashbucklerDiary/Components/MyDatePicker.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SwashbucklerDiary.Components
+{
+    public partial class MyDatePicker : DialogComponentBase
+    {
+        private bool _value;
+        private DateOnly _internalDate;
+        private DateOnly InternalDate
+        {
+            get => _internalDate;
+            set
+            {
+                _internalDate = value == DateOnly.MinValue ? DateOnly.FromDateTime(DateTime.Now) : value;
+            }
+        }
+
+        [Parameter]
+        public override bool Value
+        {
+            get => _value;
+            set => SetValue(value);
+        }
+        [Parameter]
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [Parameter]
+        public EventCallback<DateOnly> DateChanged { get; set; }
+
+        private void SetValue(bool value)
+        {
+            if (value != Value)
+            {
+                if (value)
+                {
+                    InternalDate = Date;
+                }
+                _value = value;
+            }
+        }
+
+        private async Task HandleOnOK()
+        {
+            Date = InternalDate;
+            if (DateChanged.HasDelegate)
+            {
+                await DateChanged.InvokeAsync(Date);
+            }
+            await InternalValueChanged(false);
+        }
+    }
+}
---
+++ SwashbucklerDiary/Components/PPMobileDatePicker.cs
@@ -0,0 +1,62 @@
+﻿using Masa.Blazor.Presets;
+using Microsoft.AspNetCore.Components;
+using SwashbucklerDiary.IServices;
+
+namespace SwashbucklerDiary.Components
+{
+    public class PPMobileDatePicker : PMobileDatePicker, IDisposable
+    {
+        [Inject]
+        protected INavigateService NavigateService { get; set; } = default!;
+
+        [Parameter]
+        public bool MyVisible
+        {
+            get => base.Visible;
+            set => SetVisible(value);
+        }
+        [Parameter]
+        public EventCallback<bool> MyVisibleChanged
+        {
+            get => base.VisibleChanged;
+            set => base.VisibleChanged = value;
+        }
+
+        public void Dispose()
+        {
+            if (Visible)
+            {
+                NavigateService.Action -= Close;
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        private void SetVisible(bool value)
+        {
+            if (base.Visible != value)
+            {
+                base.Visible = value;
+                Task.Run(() =>
+                {
+                    if (value)
+                    {
+                        NavigateService.Action += Close;
+                    }
+                    else
+                    {
+                        NavigateService.Action -= Close;
+                    }
+                });
+            }
+        }
+
+        private async void Close()
+        {
+            MyVisible = false;
+            if (MyVisibleChanged.HasDelegate)
+            {
+                await MyVisibleChanged.InvokeAsync(false);
+            }
+        }
+    }
+}
