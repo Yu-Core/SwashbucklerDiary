dc3f3253bc34156aa76c42a805af3a40141131cc
Yu-Core <3105136125@qq.com>
2023-07-10 13:34:38 +0800

    🐛 fix: When import,createTime wrong

--- SwashbucklerDiary/Models/Data/BaseModel.cs
+++ SwashbucklerDiary/Models/Data/BaseModel.cs
@@ -6,7 +6,7 @@ namespace SwashbucklerDiary.Models
     {
         [SugarColumn(IsPrimaryKey = true)]
         public Guid Id { get; set; }
-        [SugarColumn(InsertSql = "DATETIME('now','localtime')", IsOnlyIgnoreUpdate = true)]// getdate() now() sysdate
+        [SugarColumn(InsertSql = "DATETIME('now','localtime')")]// getdate() now() sysdate
         public DateTime CreateTime { get; set; }
         [SugarColumn(InsertSql = "DATETIME('now','localtime')", UpdateSql = "DATETIME('now','localtime')")]// getdate() now() sysdate
         public DateTime UpdateTime { get; set; }
