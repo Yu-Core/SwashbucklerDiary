2e34037fa26a8d1dbdc4494e7cfd89494bb1ed8b
Yu-Core <3105136125@qq.com>
2023-08-02 17:36:20 +0800

    🐛 fix(JistoryPage): Scroll Stuck

--- SwashbucklerDiary/wwwroot/js/scroll.js
+++ SwashbucklerDiary/wwwroot/js/scroll.js
@@ -1,10 +1,15 @@
 export function ElementVisible(dotNetCallbackRef, callbackMethod, parentSelector, childSelector) {
     var parent = document.querySelector(parentSelector);
     var child = document.querySelector(childSelector);
+    let timeout;
     parent.addEventListener('scroll', function () {
-        var parentScrollTop = parent.scrollTop;
-        var childBottom = child.offsetTop + child.offsetHeight;
-        var visible = childBottom > parentScrollTop;
-        dotNetCallbackRef.invokeMethodAsync(callbackMethod, visible);
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            var parentScrollTop = parent.scrollTop;
+            var childBottom = child.offsetTop + child.offsetHeight;
+            var visible = childBottom > parentScrollTop;
+            dotNetCallbackRef.invokeMethodAsync(callbackMethod, visible);
+        }, 200);// 设置延迟时间，单位为毫秒
+        
     });
 }
