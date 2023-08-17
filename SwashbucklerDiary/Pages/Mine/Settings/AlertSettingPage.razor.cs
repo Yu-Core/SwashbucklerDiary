8f90e4d6af65a362a76c8b7fd2372191927f3c9d
Yu-Core <3105136125@qq.com>
2023-08-17 15:30:54 +0800

    ♻️ refactor(AlertSettingPage): Change Operation Mode

--- SwashbucklerDiary/Pages/Mine/Settings/AlertSettingPage.razor.cs
+++ SwashbucklerDiary/Pages/Mine/Settings/AlertSettingPage.razor.cs
@@ -1,27 +1,45 @@
-﻿using SwashbucklerDiary.Components;
+﻿using BlazorComponent;
+using SwashbucklerDiary.Components;
 using SwashbucklerDiary.Models;
 
 namespace SwashbucklerDiary.Pages
 {
     public partial class AlertSettingPage : PageComponentBase
     {
-        private int timeout;
+        private int _timeout;
+        private bool ShowTimeout;
+        private Dictionary<string, string> Timeouts = new();
 
         protected override async Task OnInitializedAsync()
         {
+            SetTimeouts();
             await LoadSettings();
             await base.OnInitializedAsync();
         }
 
+        private StringNumber Timeout
+        {
+            get => (_timeout / 1000).ToString();
+            set => _timeout = value.ToInt32() * 1000;
+        }
+
         private async Task LoadSettings()
         {
-            timeout = await SettingsService.Get(SettingType.AlertTimeout);
+            _timeout = await SettingsService.Get(SettingType.AlertTimeout);
         }
 
         private async Task UpdateAlertTimeout(int value)
         {
-            timeout = value;
+            _timeout = value;
             await SettingsService.Save(SettingType.AlertTimeout, value);
+        }
+
+        private void SetTimeouts()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Timeouts.Add((i + 1).ToString(), string.Empty);
+            }
         }
     }
 }
