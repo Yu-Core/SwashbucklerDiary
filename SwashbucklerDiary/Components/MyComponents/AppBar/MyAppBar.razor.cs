3cc24e63deb609262a55dbf3347a7da2af4d776a
Yu-Core <3105136125@qq.com>
2023-09-12 01:55:42 +0800

    ♻️ refactor(MyAppBar): optimize ChildContent

---
+++ SwashbucklerDiary/Components/MyComponents/AppBar/MyAppBar.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SwashbucklerDiary.Components
+{
+    public partial class MyAppBar : IDisposable
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
+
+        [Parameter]
+        public string? Title { get; set; }
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= NavigationManagerOnLocationChanged;
+            GC.SuppressFinalize(this);
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            NavigationManager.LocationChanged += NavigationManagerOnLocationChanged;
+            return base.OnInitializedAsync();
+        }
+
+        private void NavigationManagerOnLocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+    }
+}
