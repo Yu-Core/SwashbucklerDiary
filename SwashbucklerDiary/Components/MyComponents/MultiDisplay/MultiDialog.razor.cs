530a2f992f4a3571ba6885e74fe4e3577dcf279c
Yu-Core <3105136125@qq.com>
2023-10-09 15:36:15 +0800

    🔥 chore: Rename MultiDialog to MultiListDialog. Rename MultiSimpleDialog to MultiDialog

--- SwashbucklerDiary/Components/MyComponents/MultiDisplay/MultiDialog.razor.cs
+++ SwashbucklerDiary/Components/MyComponents/MultiDisplay/MultiDialog.razor.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
-using SwashbucklerDiary.Models;
 
 namespace SwashbucklerDiary.Components
 {
     public partial class MultiDialog : DialogComponentBase
     {
         [Parameter]
-        public string? Title { get; set; }
-
-        [Parameter]
-        public List<DynamicListItem> DynamicListItems { get; set; } = new();
-
-        private async Task OnClick(EventCallback callback)
-        {
-            await InternalValueChanged(false);
-            await callback.InvokeAsync();
-        }
+        public RenderFragment? ChildContent { get; set; }
     }
 }
