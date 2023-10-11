c780818db4dd77bb31d712aeb68badbf9a7fc79f
Yu-Core <3105136125@qq.com>
2023-10-12 03:45:03 +0800

    Update MasaBlazor

---
+++ SwashbucklerDiary/Components/MyComponents/IntersectElement.razor.cs
@@ -0,0 +1,52 @@
+﻿using Masa.Blazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace SwashbucklerDiary.Components
+{
+    public partial class IntersectElement : IAsyncDisposable
+    {
+        private ElementReference Ref;
+
+        [Inject]
+        private IntersectJSModule IntersectJSModule { get; set; } = default!;
+
+        [Parameter]
+        public string? Class { get;set; }
+
+        [Parameter]
+        public string? Style { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> IntersectChanged { get; set; }
+
+
+        public async ValueTask DisposeAsync()
+        {
+            await IntersectJSModule.UnobserveAsync(Ref);
+            GC.SuppressFinalize(this);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var handle = DotNetObjectReference.Create(new IntersectInvoker(OnIntersectAsync));
+                await IntersectJSModule.ObserverAsync(Ref, handle);
+            }
+        }
+
+        private async Task OnIntersectAsync(IntersectEventArgs args)
+        {
+            if (IntersectChanged.HasDelegate)
+            {
+                await IntersectChanged.InvokeAsync(args.IsIntersecting);
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
--- SwashbucklerDiary/Pages/History/HistoryPage.razor.cs
+++ SwashbucklerDiary/Pages/History/HistoryPage.razor.cs
@@ -1,12 +1,10 @@
 ﻿using BlazorComponent.JSInterop;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 using SwashbucklerDiary.Components;
 using SwashbucklerDiary.Models;
 
 namespace SwashbucklerDiary.Pages
 {
-    public partial class IndexHistory : DiariesPageComponentBase
+    public partial class HistoryPage : DiariesPageComponentBase
     {
         private bool NormalCalendarVisible = true;
 
@@ -16,32 +14,12 @@ namespace SwashbucklerDiary.Pages
 
         private ScrollContainer ScrollContainer = default!;
 
-        private ElementReference NormalCalendar;
-
         private DateOnly _pickedDate = DateOnly.FromDateTime(DateTime.Now);
 
         private DateOnly[] EventsDates = Array.Empty<DateOnly>();
 
         private List<DiaryModel> PickedDiaries = new();
 
-        [JSInvokable]
-        public async Task ShowNormalCalendar(bool value)
-        {
-            NormalCalendarVisible = value;
-            await InvokeAsync(StateHasChanged);
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                var dotNetCallbackRef = DotNetObjectReference.Create(this);
-                await JS.InvokeVoidAsync("listenElementVisibility", new object[4] { dotNetCallbackRef, "ShowNormalCalendar", ScrollContainer.Ref, NormalCalendar });
-            }
-
-            await base.OnAfterRenderAsync(firstRender);
-        }
-        
         protected override async Task UpdateDiariesAsync()
         {
             var diaries = await DiaryService.QueryAsync(it => !it.Private);
@@ -125,6 +103,11 @@ namespace SwashbucklerDiary.Pages
                 && it.CreateTime.Month == PickedDate.Month
                 && it.CreateTime.Year == PickedDate.Year)
                 .ToList();
+        }
+
+        private void HandleIntersectChanged(bool value)
+        {
+            NormalCalendarVisible = value;
         }
     }
 }
