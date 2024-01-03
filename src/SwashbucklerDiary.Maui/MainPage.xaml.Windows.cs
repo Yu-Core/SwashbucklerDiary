cdebda5f066f9d360dbe1d9b87c94ae2c3a3d2e6
Yu-Core <3105136125@qq.com>
2024-01-04 04:50:52 +0800

    🆕 feat: video support full screen on Windows

--- src/SwashbucklerDiary.Maui/MainPage.xaml.Windows.cs
+++ src/SwashbucklerDiary.Maui/MainPage.xaml.Windows.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.WebView;
 using Microsoft.Maui.Platform;
+using Microsoft.UI.Windowing;
+using Microsoft.Web.WebView2.Core;
 
 namespace SwashbucklerDiary.Maui
 {
     public partial class MainPage
     {
+        private static AppWindow? AppWindow => (Application.Current?.Windows[0]?.Handler?.PlatformView as Microsoft.UI.Xaml.Window)?.AppWindow;
+
         private partial void BlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs e)
         {
             e.EnvironmentOptions = new()
@@ -21,6 +25,19 @@ namespace SwashbucklerDiary.Maui
             //https://learn.microsoft.com/en-us/dotnet/api/microsoft.web.webview2.core.corewebview2controller.defaultbackgroundcolor?view=webview2-dotnet-1.0.1370.28
 
             e.WebView.DefaultBackgroundColor = color.ToWindowsColor();
+            e.WebView.CoreWebView2.ContainsFullScreenElementChanged += FullScreen;
+        }
+
+        private void FullScreen(CoreWebView2 coreWebView2,object args)
+        {
+            if (coreWebView2.ContainsFullScreenElement)
+            {
+                AppWindow?.SetPresenter(AppWindowPresenterKind.FullScreen);
+            }
+            else
+            {
+                AppWindow?.SetPresenter(AppWindowPresenterKind.Default);
+            }
         }
     }
 }
