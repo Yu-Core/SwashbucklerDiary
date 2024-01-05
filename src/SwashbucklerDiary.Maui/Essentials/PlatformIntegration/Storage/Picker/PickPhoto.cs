030c27728953d91cebe62bc2c6b4f427c460cc32
Yu-Core <3105136125@qq.com>
2024-01-05 18:11:40 +0800

    ♻️ refactor: Optimize the pick of images and videos on mac OS

--- src/SwashbucklerDiary.Maui/Essentials/PlatformIntegration/Storage/Picker/PickPhoto.cs
+++ src/SwashbucklerDiary.Maui/Essentials/PlatformIntegration/Storage/Picker/PickPhoto.cs
@@ -1,12 +1,23 @@
-﻿namespace SwashbucklerDiary.Maui.Essentials
+﻿#if MACCATALYST
+using MobileCoreServices;
+#endif
+
+namespace SwashbucklerDiary.Maui.Essentials
 {
     public partial class PlatformIntegration
     {
         public async Task<string?> PickPhotoAsync()
         {
-            FileResult photo = await MediaPicker.Default.PickPhotoAsync();
-            var path = photo?.FullPath;
-            return path;
+#if MACCATALYST
+            string[] videoSuffixName = [".jpg", ".jpeg", ".png", ".gif", ".svg", ".webp"];
+#pragma warning disable CA1422 // 验证平台兼容性
+            string[] types = { UTType.JPEG, UTType.PNG, UTType.GIF, UTType.ScalableVectorGraphics, "webp" };
+#pragma warning restore CA1422 // 验证平台兼容性
+            return await PickFileAsync(types, videoSuffixName);
+#else
+            FileResult fileResult = await MediaPicker.Default.PickPhotoAsync();
+            return fileResult?.FullPath;
+#endif
         }
     }
 }
--- src/SwashbucklerDiary.Maui/Essentials/PlatformIntegration/Storage/Picker/PickVideo.cs
+++ src/SwashbucklerDiary.Maui/Essentials/PlatformIntegration/Storage/Picker/PickVideo.cs
@@ -1,11 +1,23 @@
-﻿namespace SwashbucklerDiary.Maui.Essentials
+﻿#if MACCATALYST
+using MobileCoreServices;
+#endif
+
+namespace SwashbucklerDiary.Maui.Essentials
 {
     public partial class PlatformIntegration
     {
         public async Task<string?> PickVideoAsync()
         {
-            FileResult photo = await MediaPicker.Default.PickVideoAsync();
-            return photo?.FullPath;
+#if MACCATALYST
+            string[] videoSuffixName = [".mp4", ".m4v", ".mpg", ".mpeg", ".mp2", ".mov", ".avi", ".mkv", ".flv", ".gifv", ".qt"];
+#pragma warning disable CA1422 // 验证平台兼容性
+            string[] types = { UTType.MPEG4, UTType.Video, UTType.AVIMovie, UTType.AppleProtectedMPEG4Video, "mp4", "m4v", "mpg", "mpeg", "mp2", "mov", "avi", "mkv", "flv", "gifv", "qt" };
+#pragma warning restore CA1422 // 验证平台兼容性
+            return await PickFileAsync(types, videoSuffixName);
+#else
+            FileResult fileResult = await MediaPicker.Default.PickVideoAsync();
+            return fileResult?.FullPath;
+#endif
         }
     }
 }
