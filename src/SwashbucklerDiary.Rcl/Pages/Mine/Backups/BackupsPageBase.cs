d91548431c4c0484292b43df21550f445ce3ded3
Yu-Core <3105136125@qq.com>
2024-02-04 12:52:59 +0800

    🔥 chore: update .gitignore

---
+++ src/SwashbucklerDiary.Rcl/Pages/Mine/Backups/BackupsPageBase.cs
@@ -0,0 +1,36 @@
+﻿using BlazorComponent;
+using Microsoft.AspNetCore.Components;
+using SwashbucklerDiary.Rcl.Components;
+
+namespace SwashbucklerDiary.Rcl.Pages
+{
+    public abstract class BackupsPageBase : ImportantComponentBase
+    {
+        protected StringNumber tab = 0;
+
+        protected readonly List<string> views = new() { "Local", "WebDAV" };
+
+        protected readonly List<string> tabNames = ["Backups.Local.Name", "Backups.WebDAV.Name"];
+
+        [Parameter]
+        [SupplyParameterFromQuery]
+        public string? View { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            InitTab();
+        }
+
+        protected void InitTab()
+        {
+            if (string.IsNullOrEmpty(View))
+            {
+                View = views[0];
+            }
+
+            tab = views.IndexOf(View!);
+        }
+    }
+}
