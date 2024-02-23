e275529154294140db3f27555c1802c344ac36f3
Yu-Core <3105136125@qq.com>
2024-02-23 20:37:02 +0800

    ♻️ refactor(DiaryCard): Move Extract to Extension Method

--- src/SwashbucklerDiary.Rcl/Components/MyComponents/DiaryCard/DiaryCard.razor.cs
+++ src/SwashbucklerDiary.Rcl/Components/MyComponents/DiaryCard/DiaryCard.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using SwashbucklerDiary.Rcl.Extensions;
 using SwashbucklerDiary.Rcl.Services;
 using SwashbucklerDiary.Shared;
 
@@ -14,7 +15,7 @@ namespace SwashbucklerDiary.Rcl.Components
 
         private string? moodIcon;
 
-        private DiaryModel previousValue = new();
+        private Guid previousValueId;
 
         [Inject]
         private IIconService IconService { get; set; } = default!;
@@ -33,118 +34,24 @@ namespace SwashbucklerDiary.Rcl.Components
 
         private bool ShowMenu => DiaryCardList.ShowMenu;
 
-        private string ValueContent => Value.Content ?? string.Empty;
-
-        private bool IsTop => Value.Top;
-
         private bool ShowIcon => DiaryCardList.ShowIcon;
 
         private string? DateFormat => DiaryCardList.DateFormat;
 
         private string? Date => DateFormat is null ? null : Value.CreateTime.ToString(DateFormat);
 
-        private string? CreateDiaryTitle()
-        {
-            if (!string.IsNullOrWhiteSpace(Value.Title))
-            {
-                return Value.Title;
-            }
-            else
-            {
-                return CreateTitleFormText();
-            }
-        }
-
-        private string? CreateDiaryText()
-        {
-            string text = SubText(Value.Content, 0, 2000);
-            if (string.IsNullOrWhiteSpace(Value.Title))
-            {
-                int index = ExtractTitleIndex();
-                var subText = SubText(text, index + 1);
-                if (subText != string.Empty)
-                {
-                    return subText;
-                }
-            }
-
-            return text;
-        }
-
-        private static string SubText(string? text, int startIndex, int? length = null)
-        {
-            if (text == null)
-            {
-                return string.Empty;
-            }
-
-            int textLength = text.Length;
-
-            if (startIndex < 0 || startIndex >= textLength)
-            {
-                return string.Empty;
-            }
-
-            if (length == null)
-            {
-                length = textLength - startIndex;
-            }
-            else if (length < 0)
-            {
-                return string.Empty;
-            }
-
-            int endIndex = startIndex + length.Value;
-
-            if (endIndex > textLength)
-            {
-                endIndex = textLength;
-            }
-
-            return text.Substring(startIndex, endIndex - startIndex);
-        }
-
         private void ToRead()
         {
             NavigateService.PushAsync($"read?Id={Value.Id}");
         }
 
-        private string CreateTitleFormText()
-        {
-            int index = ExtractTitleIndex();
-            if (index > -1)
-            {
-                return ValueContent.Substring(0, index + 1);
-            }
-            else
-            {
-                return SubText(ValueContent, 0, 200);
-            }
-        }
-
-        private int ExtractTitleIndex()
-        {
-            char[] separators = [']', '。', '\n']; // 定义分隔符
-            int index = ValueContent.IndexOfAny(separators);
-            if (index == -1 || index == ValueContent.Length - 1)
-            {
-                index = ValueContent.IndexOf(". ");
-                if (index > -1)
-                {
-                    index++;
-                }
-            }
-
-            return index != ValueContent.Length - 1 ? index : -1;
-        }
-
         private void SetContent()
         {
-            if (previousValue.Id != Value.Id)
+            if (previousValueId != Value.Id)
             {
-                previousValue = Value;
-                title = CreateDiaryTitle();
-                text = CreateDiaryText();
+                previousValueId = Value.Id;
+                title = Value.ExtractTitle();
+                text = Value.ExtractText();
                 weatherIcon = string.IsNullOrWhiteSpace(Value.Weather) ? null : IconService.GetWeatherIcon(Value.Weather);
                 moodIcon = string.IsNullOrWhiteSpace(Value.Mood) ? null : IconService.GetMoodIcon(Value.Mood);
             }
--- src/SwashbucklerDiary.Rcl/Extensions/DiaryModelExtensions.cs
+++ src/SwashbucklerDiary.Rcl/Extensions/DiaryModelExtensions.cs
@@ -13,5 +13,100 @@ namespace SwashbucklerDiary.Rcl.Extensions
 
             return diary.Title + "\n" + diary.Content;
         }
+
+        public static string? ExtractTitle(this DiaryModel diary)
+        {
+            if (!string.IsNullOrWhiteSpace(diary.Title))
+            {
+                return diary.Title;
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(diary.Content))
+                {
+                    return string.Empty;
+                }
+
+                int index = ExtractTitleIndex(diary.Content);
+                if (index > -1)
+                {
+                    return diary.Content.Substring(0, index + 1);
+                }
+                else
+                {
+                    return SubText(diary.Content, 0, 200);
+                }
+            }
+        }
+
+        public static string? ExtractText(this DiaryModel diary)
+        {
+            if (string.IsNullOrWhiteSpace(diary.Content))
+            {
+                return string.Empty;
+            }
+
+            string text = SubText(diary.Content, 0, 2000);
+            if (string.IsNullOrWhiteSpace(diary.Title))
+            {
+                int index = ExtractTitleIndex(diary.Content);
+                var subText = SubText(text, index + 1);
+                if (subText != string.Empty)
+                {
+                    return subText;
+                }
+            }
+
+            return text;
+        }
+
+        private static string SubText(string? text, int startIndex, int? length = null)
+        {
+            if (text == null)
+            {
+                return string.Empty;
+            }
+
+            int textLength = text.Length;
+
+            if (startIndex < 0 || startIndex >= textLength)
+            {
+                return string.Empty;
+            }
+
+            if (length == null)
+            {
+                length = textLength - startIndex;
+            }
+            else if (length < 0)
+            {
+                return string.Empty;
+            }
+
+            int endIndex = startIndex + length.Value;
+
+            if (endIndex > textLength)
+            {
+                endIndex = textLength;
+            }
+
+            return text.Substring(startIndex, endIndex - startIndex);
+        }
+
+        private static int ExtractTitleIndex(string text)
+        {
+            char[] separators = [']', '。', '\n']; // 定义分隔符
+            int index = text.IndexOfAny(separators);
+            if (index == -1 || index == text.Length - 1)
+            {
+                index = text.IndexOf(". ");
+                if (index > -1)
+                {
+                    index++;
+                }
+            }
+
+            return index != text.Length - 1 ? index : -1;
+        }
     }
 }
