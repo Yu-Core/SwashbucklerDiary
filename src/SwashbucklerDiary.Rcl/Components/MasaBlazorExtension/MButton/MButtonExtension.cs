38833a559875a51cfeb0f6b66b8a36f412a88d25
Yu-Core <3105136125@qq.com>
2024-03-21 00:28:30 +0800

    ♻️ refactor: restore MButtonExtension

---
+++ src/SwashbucklerDiary.Rcl/Components/MasaBlazorExtension/MButton/MButtonExtension.cs
@@ -0,0 +1,22 @@
+﻿using Masa.Blazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace SwashbucklerDiary.Rcl.Components
+{
+    public class MButtonExtension : MButton
+    {
+        [Parameter]
+        public bool OnMousedownPreventDefault { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender && OnMousedownPreventDefault)
+            {
+                await Js.InvokeVoidAsync("preventDefaultOnmousedown", Ref);
+            }
+        }
+    }
+}
--- src/SwashbucklerDiary.Rcl/wwwroot/js/utility.js
+++ src/SwashbucklerDiary.Rcl/wwwroot/js/utility.js
@@ -1,3 +1,13 @@
+function preventDefaultOnmousedown(el) {
+    if (el == null) {
+        return;
+    }
+
+    el.onmousedown = (e) => {
+        e.preventDefault();
+    }
+}
+
 function changeLanguage(lang) {
     document.documentElement.lang = lang;
 }
