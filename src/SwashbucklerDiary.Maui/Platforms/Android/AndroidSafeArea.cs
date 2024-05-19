360ba8888c08506f9997ef49abec89758849c954
Yu-Core <3105136125@qq.com>
2024-05-19 23:09:06 +0800

    💄 style: update safe-area style

--- src/SwashbucklerDiary.Maui/Platforms/Android/AndroidSafeArea.cs
+++ src/SwashbucklerDiary.Maui/Platforms/Android/AndroidSafeArea.cs
@@ -23,30 +23,31 @@ namespace SwashbucklerDiary.Maui
 
         private static void SetSafeAreaCss(Android.Webkit.WebView webView)
         {
-            int statusBarHeight = Utilities.GetStatusBarInsets().Top;
+            int safeAreaInsetTop = Utilities.GetStatusBarInsets().Top;
             AndroidX.Core.Graphics.Insets navigationBarInsets = Utilities.GetNavigationBarInsets();
-            int navigationBarHeight;
+            int safeAreaInsetBottom;
             // DeviceDisplay.Current.MainDisplayInfo.Orientation is inaccurate first entry, It must be Portrait
             if (DeviceDisplay.Current.MainDisplayInfo.Width > DeviceDisplay.Current.MainDisplayInfo.Height)
             {
-                navigationBarHeight = 0;
+                safeAreaInsetBottom = 0;
             }
             else
             {
-                navigationBarHeight = navigationBarInsets.Bottom;
+                safeAreaInsetBottom = navigationBarInsets.Bottom;
             }
-            int leftSafeAreaWidth = navigationBarInsets.Left;
-            int rightSafeAreaWidth = navigationBarInsets.Right;
-            SetSafeAreaCss(webView, statusBarHeight, navigationBarHeight, leftSafeAreaWidth, rightSafeAreaWidth);
+
+            int safeAreaInsetLeft = navigationBarInsets.Left;
+            int safeAreaInsetRight = navigationBarInsets.Right;
+            SetSafeAreaCss(webView, safeAreaInsetTop, safeAreaInsetBottom, safeAreaInsetLeft, safeAreaInsetRight);
         }
 
-        private static void SetSafeAreaCss(Android.Webkit.WebView webView, int statusBarHeight, int navigationBarHeight, int leftSafeAreaWidth, int rightSafeAreaWidth)
+        private static void SetSafeAreaCss(Android.Webkit.WebView webView, int safeAreaInsetTop, int safeAreaInsetBottom, int safeAreaInsetLeft, int safeAreaInsetRight)
         {
             webView.EvaluateJavascript(@$"
-                document.documentElement.style.setProperty('--android-status-bar-height','{Utilities.PxToDip(statusBarHeight)}px');
-                document.documentElement.style.setProperty('--android-navigation-bar-height','{Utilities.PxToDip(navigationBarHeight)}px');                
-                document.documentElement.style.setProperty('--android-safe-area-left-width','{Utilities.PxToDip(leftSafeAreaWidth)}px');
-                document.documentElement.style.setProperty('--android-safe-area-right-width','{Utilities.PxToDip(rightSafeAreaWidth)}px');
+                document.documentElement.style.setProperty('--safe-area-inset-top','{Utilities.PxToDip(safeAreaInsetTop)}px');
+                document.documentElement.style.setProperty('--safe-area-inset-bottom','{Utilities.PxToDip(safeAreaInsetBottom)}px');                
+                document.documentElement.style.setProperty('--safe-area-inset-left','{Utilities.PxToDip(safeAreaInsetLeft)}px');
+                document.documentElement.style.setProperty('--safe-area-inset-right','{Utilities.PxToDip(safeAreaInsetRight)}px');
             ", null);
         }
     }
