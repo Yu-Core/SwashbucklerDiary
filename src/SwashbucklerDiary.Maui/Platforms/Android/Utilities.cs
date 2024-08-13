2d7aa5cdd68ede4061ff6e910998133e917d4f18
Yu-Core <3105136125@qq.com>
2024-08-13 22:33:44 +0800

    🐛 fix: Possible crashes caused by soft keyboard retraction and loading when inserting files

--- src/SwashbucklerDiary.Maui/Platforms/Android/Utilities.cs
+++ src/SwashbucklerDiary.Maui/Platforms/Android/Utilities.cs
@@ -6,10 +6,17 @@ namespace SwashbucklerDiary.Maui
 #nullable disable
     public static class Utilities
     {
+        private static readonly AndroidX.Core.Graphics.Insets emptyInsets = AndroidX.Core.Graphics.Insets.Of(0, 0, 0, 0);
+
         public static AndroidX.Core.Graphics.Insets GetStatusBarInsets()
         {
             Android.Views.View decorView = Platform.CurrentActivity.FindViewById(Id.Content);
             WindowInsetsCompat windowInsets = ViewCompat.GetRootWindowInsets(decorView);
+            if (windowInsets is null)
+            {
+                return emptyInsets;
+            }
+
             return windowInsets.GetInsets(WindowInsetsCompat.Type.StatusBars());
         }
 
@@ -17,6 +24,11 @@ namespace SwashbucklerDiary.Maui
         {
             Android.Views.View decorView = Platform.CurrentActivity.FindViewById(Id.Content);
             WindowInsetsCompat windowInsets = ViewCompat.GetRootWindowInsets(decorView);
+            if (windowInsets is null)
+            {
+                return emptyInsets;
+            }
+
             return windowInsets.GetInsets(WindowInsetsCompat.Type.NavigationBars());
         }
 
