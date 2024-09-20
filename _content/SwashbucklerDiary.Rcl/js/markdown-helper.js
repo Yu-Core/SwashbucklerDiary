667d3f84788c48703802a3695d6120b3a2746dc1
Yu-Core <Yu-Core@users.noreply.github.com>
2024-09-20 15:16:41 +0000

    deploy: f7512f8a4ba2dde65e166f853107529f6866a189

---
+++ _content/SwashbucklerDiary.Rcl/js/markdown-helper.js
@@ -0,0 +1,59 @@
+﻿export function after(domRef) {
+    handleToolbar(domRef);
+}
+
+export function focus(domRef) {
+    domRef.Vditor.focus();
+}
+
+export function autofocus(domRef) {
+    domRef.Vditor.focus();
+    const focusElement = document.activeElement;
+    let range = document.createRange();
+    range.selectNodeContents(focusElement);
+    range.collapse(false);
+    let sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
+
+export function upload(element, inputFileElement) {
+    if (!element || !inputFileElement) return;
+    const uploadElement = element.querySelector('input[type=file]');
+    inputFileElement.files = uploadElement.files;
+    inputFileElement.dispatchEvent(new CustomEvent('change'));
+}
+
+function handleToolbar(domRef) {
+    if (!domRef) {
+        return;
+    }
+    //点击Vditor工具栏，输入框不失去焦点
+    var toolbar = domRef.querySelector(".vditor-toolbar");
+    if (!toolbar) {
+        console.log("Vditor toolbar does not exist");
+        return;
+    }
+    //prevent Input Lose Focus
+    toolbar.onmousedown = (e) => {
+        e.preventDefault();
+    };
+
+    const toolbarContent = document.createElement("div");
+    toolbarContent.classList.add('vditor-toolbar-content');
+    insertMiddleElement(toolbar, toolbarContent);
+    const items = toolbarContent.querySelectorAll('.vditor-toolbar__item');
+    const visibleItems = Array.from(items).filter(item => getComputedStyle(item).display !== 'none');
+    if (visibleItems.length > 0) {
+        const itemWidth = parseInt(getComputedStyle(visibleItems[0]).width);
+        const vditorToolbarWidth = itemWidth * Math.round(visibleItems.length / 2) + 'px';
+        toolbarContent.style.setProperty('--vditor-toolbar-width', vditorToolbarWidth);
+    }
+}
+
+function insertMiddleElement(parent, destination) {
+    while (parent.firstChild) {
+        destination.appendChild(parent.firstChild);
+    }
+    parent.appendChild(destination);
+}
