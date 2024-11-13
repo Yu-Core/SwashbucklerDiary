7e17d9b5d947b6c0ba7402f6bb4695ae3046f4aa
Yu-Core <3105136125@qq.com>
2024-11-13 21:12:02 +0800

    ♻️ refactor(ErrorDialog): Add Waiting

---
+++ src/SwashbucklerDiary.Rcl/Components/DialogComponents/ErrorDialog.razor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components;
+using SwashbucklerDiary.Rcl.Essentials;
+using SwashbucklerDiary.Rcl.Services;
+
+namespace SwashbucklerDiary.Rcl.Components
+{
+    public partial class ErrorDialog
+    {
+        private int timeRemaining = 3;
+        private bool isCountingDown = true;
+        private PeriodicTimer? timer;
+
+        [Inject]
+        private II18nService I18n { get; set; } = default!;
+
+        [Inject]
+        private IAppLifecycle AppLifecycle { get; set; } = default!;
+
+        [CascadingParameter(Name = "Culture")]
+        public string? Culture { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                await StartCountdown();
+            }
+        }
+
+        private string Text => isCountingDown
+            ? $"{I18n.T("ErrorHandle.WaitContent")}{timeRemaining}s"
+            : I18n.T("ErrorHandle.Content");
+
+        private void Quit()
+        {
+            AppLifecycle.QuitApp();
+        }
+
+        private async Task StartCountdown()
+        {
+            isCountingDown = true;
+
+            timer = new PeriodicTimer(TimeSpan.FromSeconds(1)); // 每秒触发一次
+
+            try
+            {
+                while (await timer.WaitForNextTickAsync())
+                {
+                    timeRemaining--;
+
+                    if (timeRemaining > 0)
+                    {
+                        StateHasChanged(); // 更新 UI
+                    }
+                    else
+                    {
+                        isCountingDown = false;
+                        StateHasChanged();
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                timer?.Dispose(); // 确保定时器被释放
+            }
+        }
+    }
+}
