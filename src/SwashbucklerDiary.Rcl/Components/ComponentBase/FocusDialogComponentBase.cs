851d4dea922c8364ad816d75032089a5b38f0134
Yu-Core <3105136125@qq.com>
2024-12-11 19:53:35 +0800

    ♻️ refactor(Dialog): Add DisabAutoFocus to a dialog with an input box

--- src/SwashbucklerDiary.Rcl/Components/ComponentBase/FocusDialogComponentBase.cs
+++ src/SwashbucklerDiary.Rcl/Components/ComponentBase/FocusDialogComponentBase.cs
@@ -7,6 +7,7 @@ namespace SwashbucklerDiary.Rcl.Components
     {
         protected MTextField<string?> textField = default!;
 
+        // MTextField AutoFocus is too fast, Soft keyboards may experience lag
         protected virtual async Task FocusAsync()
         {
             if (textField is not null && !textField.IsFocused)
