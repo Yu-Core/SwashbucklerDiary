cd22d781fb75693638bc4aa56bfafce64d560bcd
Yu-Core <3105136125@qq.com>
2025-05-01 00:03:54 +0800

    ♻️ refactor(ErrorDialog) : display log

--- src/SwashbucklerDiary.Rcl/Components/DialogComponents/ErrorDialog.razor.cs
+++ src/SwashbucklerDiary.Rcl/Components/DialogComponents/ErrorDialog.razor.cs
@@ -6,66 +6,38 @@ namespace SwashbucklerDiary.Rcl.Components
 {
     public partial class ErrorDialog
     {
-        private int timeRemaining = 3;
-        private bool isCountingDown = true;
-        private PeriodicTimer? timer;
-
         [Inject]
         private II18nService I18n { get; set; } = default!;
 
         [Inject]
         private IAppLifecycle AppLifecycle { get; set; } = default!;
 
+        [Inject]
+        private IPlatformIntegration PlatformIntegration { get; set; } = default!;
+
+        [Inject]
+        private IAlertService AlertService { get; set; } = default!;
+
         [CascadingParameter(Name = "Culture")]
         public string? Culture { get; set; }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            await base.OnAfterRenderAsync(firstRender);
+        [Parameter]
+        public Exception? Exception { get; set; }
 
-            if (firstRender)
+        private string? Text => Exception is null ? null : $"{Exception.Message}\n{Exception.StackTrace}";
+
+        private async Task CopyDetailedInformationAsync()
+        {
+            if (Text is not null)
             {
-                await StartCountdown();
+                await PlatformIntegration.SetClipboardAsync(Text);
+                await AlertService.Success(I18n.T("Copy successfully"));
             }
         }
-
-        private string Text => isCountingDown
-            ? $"{I18n.T("An abnormality occurred. It is being saved to the log. Please wait...")}{timeRemaining}s"
-            : I18n.T("An abnormal situation has occurred. It has been saved to the log. Please exit the application.");
 
         private void Quit()
         {
             AppLifecycle.QuitApp();
-        }
-
-        private async Task StartCountdown()
-        {
-            isCountingDown = true;
-
-            timer = new PeriodicTimer(TimeSpan.FromSeconds(1)); // 每秒触发一次
-
-            try
-            {
-                while (await timer.WaitForNextTickAsync())
-                {
-                    timeRemaining--;
-
-                    if (timeRemaining > 0)
-                    {
-                        StateHasChanged(); // 更新 UI
-                    }
-                    else
-                    {
-                        isCountingDown = false;
-                        StateHasChanged();
-                        break;
-                    }
-                }
-            }
-            finally
-            {
-                timer?.Dispose(); // 确保定时器被释放
-            }
         }
     }
 }
