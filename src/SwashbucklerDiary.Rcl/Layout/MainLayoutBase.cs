0151645056e4e7b1a06e50150b54970f50b9336d
Yu-Core <3105136125@qq.com>
2025-11-05 00:18:15 +0800

    🐛 fix: Exception occurred when changing the theme during backup or import recovery

--- src/SwashbucklerDiary.Rcl/Layout/MainLayoutBase.cs
+++ src/SwashbucklerDiary.Rcl/Layout/MainLayoutBase.cs
@@ -133,11 +133,14 @@ namespace SwashbucklerDiary.Rcl.Layout
 
         protected void HandleSettingsChanged()
         {
-            var theme = SettingService.Get(it => it.Theme);
-            ThemeService.SetTheme(theme);
-            // A bug, it will cause the language selection RadioDialog to fail to select
-            //var language = SettingService.Get(it => it.Language);
-            //I18n.SetCulture(language);
+            InvokeAsync(() =>
+            {
+                var theme = SettingService.Get(it => it.Theme);
+                ThemeService.SetTheme(theme);
+
+                var language = SettingService.Get(it => it.Language);
+                I18n.SetCulture(new(language));
+            });
         }
 
         private async Task SponsorSupport()
